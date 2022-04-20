f0023d57747e348c66373625c0681f33ea01c797
Bruno Borges <76704076+DEV-Borges@users.noreply.github.com>
2022-04-20 17:05:41 -0300

    Update script.js

--- Jogo_da_velha/script.js
+++ Jogo_da_velha/script.js
@@ -2,14 +2,14 @@ var jogador = null;
 var jogadorSelecionado = document.getElementById("jogador-selecionado")//pegando a tag label com o ID="jogador-selecionado"
 var quad = document.getElementsByClassName('quadrado');//pegando todas as divs com a class="quadrado"
 
-mudarJogador('X');//Dando valor inicial "X" para 
+mudarJogador('X');//Dando valor inicial "X" para variavel jogador.
 
 ///////Preenche a div com "X" ou "O".
 function selecionarQuad(id){
 
     var quad = document.getElementById(id);//pegando a div do html para fazer a comparação
     
-    //Verificando se a div esta com o text "-" para da continuidade á função "selecionarQuad()"
+    //Verificando se a div esta com o text "-" para da continuidade á função "selecionarQuad()".
     if (quad.innerHTML !== '-') {
         return;
     }
@@ -28,12 +28,11 @@ function selecionarQuad(id){
 
 }
 
-///Função para indicar de quem é a vez e para preencher a div com o parametro "valor".
+///Função para indicar de quem é a vez, para preencher a div com o parametro "valor".
 function mudarJogador(valor) {
     jogador = valor
     jogadorSelecionado.innerHTML = jogador
 }
-
 
 function checkVencedor() {
     
